0293319b0ceead0a27572f2c9809a0659772e7c8
IvanJirki <c3jiiv00@students.oamk.fi>
2024-10-16 01:47:23 +0300

    uudistettu ja component osio valmis

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
         <div className="app-container">
             <header className="header-container">
                 <img src={logo} alt="Product Order" className="header-image" />
-                <h1>Welcome to Hulk page</h1>
+                <h1>Welcome to Big man page</h1>
             </header>
             <OrderForm />
         </div>
